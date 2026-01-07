302b69515410ed50a732db3283d7a2862c182f9f
Elisha Riedlinger <elisha@novicemail.com>
2026-01-07 15:25:41 -0800

    Use smallest epsilon number for DepthBias (#214)

--- source/d3d8types.cpp
+++ source/d3d8types.cpp
@@ -51,14 +51,15 @@ DWORD CalcDepthBias(DWORD ZBias, DWORD DepthBitCount)
 	{
 	case 32:
 	case 24:
-		DepthEpsilon = -10.0f / (1 << 16);
+		// Bit shifting by 24 is too small for float precision. A shift of 20 seems to work best.
+		DepthEpsilon = -1.0f / ((1 << 20) - 1);
 		break;
 	default:
 	case 16:
-		DepthEpsilon = -20.0f / (1 << 16);
+		DepthEpsilon = -1.0f / ((1 << 16) - 1);
 		break;
 	case 15:
-		DepthEpsilon = -25.0f / (1 << 16);
+		DepthEpsilon = -1.0f / ((1 << 15) - 1);
 		break;
 	}
 	float DepthBias = std::min(ZBias, 16UL) * DepthEpsilon;
